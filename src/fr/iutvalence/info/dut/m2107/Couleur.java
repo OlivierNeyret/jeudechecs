ad786fa380f1f146774f51abe762201354fc2cdc
pasquiop <pasquiop@PC-B123-07.iut-valence.fr>
2016-04-28 13:00:15 +0200

    Ajout des classes Plateau et couleur

---
+++ src/fr/iutvalence/info/dut/m2107/Couleur.java
@@ -0,0 +1,18 @@
+package fr.iutvalence.info.dut.m2107;
+
+/**
+ * toutes les couleurs possibles des pieces
+ * @author pasquiop
+ *
+ */
+public enum Couleur
+{
+	/**
+	 * represente la couleur banche
+	 */
+	Blanc,
+	/**
+	 * represente la couleur noire
+	 */
+	Noir;
+}
