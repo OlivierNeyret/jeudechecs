145401728c6e3968f551012647dc12d50b2448af
Xavier <Xavier@192.168.1.19>
2016-04-28 16:35:59 +0200

    Commentaires des classes Deplacement,Difficulte et IA, et de leurs attributs.

--- src/fr/iutvalence/info/dut/m2107/Deplacement.java
+++ src/fr/iutvalence/info/dut/m2107/Deplacement.java
@@ -1,8 +1,37 @@
 package fr.iutvalence.info.dut.m2107;
 
+/**
+ * Classe qui definit un deplacement par les coordonnees d'arrivee de la piece
+ * @author Xavier
+ *
+ */
 public class Deplacement {
 
+	/**
+	 * Absisse d'arrivee de la piece
+	 */
 	private int abscisse;
+	
+	/**
+	 * Ordonnee d'arrivee de la piece
+	 */
 	private int ordonnee;
+
+	/**
+	 * @return renvoie l'abscisse du deplacement
+	 */
+	public int getAbscisse()
+	{
+		return this.abscisse;
+	}
+
+
+	/**
+	 * @return renvoie l'ordonnee du deplacement
+	 */
+	public int getOrdonnee()
+	{
+		return this.ordonnee;
+	}
 
 }
--- src/fr/iutvalence/info/dut/m2107/IA.java
+++ src/fr/iutvalence/info/dut/m2107/IA.java
@@ -1,12 +1,20 @@
 package fr.iutvalence.info.dut.m2107;
+
+/**
+ * Classe qui gere la façon dont l'intelligence artificielle (IA) va jouer
+ * @author Xavier
+ *
+ */
 public class IA extends Joueur {
 
 	private Difficulte difficulte;
+	
 
 	/**
-	 * 
-	 * @param Piece
-	 * @param Deplacement
+	 * Redefinition de Joueur.deplacer dans laquelle l'IA choisira le deplacement en
+	 * fonction de la difficulté et de l'état de la partie
+	 * @param Piece La piece a deplacer
+	 * @param Deplacement Le deplacement que la piece doit effectuer
 	 */
 	public void deplacer(int Piece, int Deplacement) {
 		// TODO - implement IA.deplacer
